ef73dd3387fc75cac6361fa6a322079c8b1672cd
jonnyy04 <151456268+jonnyy04@users.noreply.github.com>
2026-05-03 19:06:55 +0300

    feat: Tailwind configuration

--- IncomeAnalyzer/src/App.jsx
+++ IncomeAnalyzer/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 underline">
+        Tailwind v4 is Ready!
+      </h1>
+    </div>
   )
 }
 
